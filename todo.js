df31e52970d9fd7eff3b969aa7b8fccef2575de0
vasu-project <164885645+vasu-project@users.noreply.github.com>
2024-04-27 23:06:46 -0700

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,25 @@
+document.querySelector('#push').onclick = function(){
+    if(document.querySelector('#newtask input').value.length == 0){
+        alert("Please Enter a Task")
+    }
+    else{
+        document.querySelector('#tasks').innerHTML += `
+            <div class="task">
+                <span id="taskname">
+                    ${document.querySelector('#newtask input').value+"🚀"}
+                </span>
+                <button class="delete">
+                    Delete🗑️
+                </button>
+            </div>
+        `;
+
+        var current_tasks = document.querySelectorAll(".delete");
+        for(var i=0; i<current_tasks.length; i++){
+            current_tasks[i].onclick = function(){
+                this.parentNode.remove();
+            }
+        }
+
+    }
+}
